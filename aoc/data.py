26bc0aabbc1d7fa78e4f0a5dae66b3cd619c766e
kruzaavn <aleks.kruza@gmail.com>
2023-05-23 16:07:31 -0400

    fun

---
+++ aoc/data.py
@@ -0,0 +1,12 @@
+import requests
+import pathlib
+
+session_id = pathlib.Path().cwd().joinpath('session.txt').read_text()
+
+
+def get_data(year, day):
+
+    response = requests.get(f'https://www.adventofcode.com/{year}/day/{day}/input', cookies={'session': session_id})
+
+    return response.content.decode('utf-8')
+
