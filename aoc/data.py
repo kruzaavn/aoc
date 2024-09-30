ecd59a5e119d07b8a3d853a92dbd052389def2e9
AleksandrsKruza <Akruza@groupw.com>
2024-09-30 13:22:28 -0400

    mo-money mo-problems

--- aoc/data.py
+++ aoc/data.py
@@ -4,9 +4,16 @@ import pathlib
 session_id = pathlib.Path().cwd().joinpath('session.txt').read_text().strip()
 
 
+class BadConnectionError(Exception):
+    pass
+
+
 def get_data(year, day):
 
     response = requests.get(f'https://www.adventofcode.com/{year}/day/{day}/input', cookies={'session': session_id})
+
+    if response.status_code != 200:
+        raise BadConnectionError(response.content.decode('utf-8'))
 
     return response.content.decode('utf-8')
 
